e494a2f9effe6b9e274878f97a8b4dd96b3b1b4b
Arash <arash.arghavan@gmail.com>
2019-03-07 17:03:40 -0600

    half way done

--- weather_map.js
+++ weather_map.js
@@ -13,16 +13,32 @@ $(function () {
         // console.log(data.currently.humidity);
         // console.log(data.currently.summary);
         // console.log(data.currently.icon);
-        console.log(data.daily.data[0].summary);
+
+        // var icon0 = (data.daily.data[0].icon);
+        // console.log(icon0);
 
 // first
 
-        $("#location").html(data.timezone).append(" latitude:" +  data.latitude).append(" longitude:" +data.longitude);
+        $("#location").html(data.timezone).append(" latitude:" + data.latitude).append(" longitude:" + data.longitude);
         $("#temperature").html("temperature: " + data.daily.data[0].apparentTemperatureHigh);
         $("#pressure").html("pressure: " + data.daily.data[0].pressure);
         $("#windSpeed").html("windSpeed: " + data.daily.data[0].windSpeed);
         $("#humidity").html("humidity: " + data.daily.data[0].humidity);
         $("#summary").html(data.daily.data[0].summary);
+
+// icon0
+        var icon0 = (data.daily.data[0].icon);
+
+        if (icon0 === 'partly-cloudy-day' || icon0 === 'cloudy' || icon0 === 'fog' || icon0 === 'partly-cloudy-night') {
+            $("#icon").prepend('<img src="cloudy.png" />');
+        }  if (icon0 === 'rain' ){
+            $("#icon").prepend('<img src="cloudy.png" />');
+        } if (icon0 === 'sun'){
+            $("#icon").prepend('<img src="sun.png" />');
+        } if (icon0 === 'rain') {
+            $("#icon").prepend('<img src="rain.png" />');
+        }
+
 
 // second
 
@@ -32,6 +48,18 @@ $(function () {
         $("#humidity1").html("humidity: " + data.daily.data[1].humidity);
         $("#summary1").html(data.daily.data[1].summary);
 
+
+// icon1
+        var icon1 = (data.daily.data[1].icon);
+
+        if (icon1 === 'partly-cloudy-day' || icon1 === 'cloudy' || icon1 === 'fog' || icon1 === 'partly-cloudy-night') {
+            $("#icon1").prepend('<img src="cloudy.png" />');
+        }  if (icon1 === 'rain'){
+            $("#icon1").prepend('<img src="rain.png" />');
+        } if (icon1 === 'sun'){
+            $("#icon1").prepend('<img src="sun.png" />');
+        }
+
 // third
 
         $("#temperature2").html("temperature: " + data.daily.data[2].apparentTemperatureHigh);
@@ -39,6 +67,18 @@ $(function () {
         $("#windSpeed2").html("windSpeed: " + data.daily.data[2].windSpeed);
         $("#humidity2").html("humidity: " + data.daily.data[2].humidity);
         $("#summary2").html(data.daily.data[2].summary);
+
+
+// icon2
+        var icon2 = (data.daily.data[2].icon);
+
+        if (icon2 === 'partly-cloudy-day' || icon2 === 'cloudy' || icon2 === 'fog' || icon2 === 'partly-cloudy-night') {
+            $("#icon2").prepend('<img src="cloudy.png" />');
+        }  if (icon2 === 'rain' ){
+            $("#icon2").prepend('<img src="rain.png" />');
+        } if (icon2 === 'sun'){
+            $("#icon2").prepend('<img src="sun.png" />');
+        }
 
     });
 
