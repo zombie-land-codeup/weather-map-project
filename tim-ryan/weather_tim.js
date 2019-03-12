de386f0da87987cec7f9325b1484874868a1813a
Timryan133 <Tim.ryan133@gmail.com>
2019-03-12 10:38:59 -0500

    small changes

--- tim-ryan/weather_tim.js
+++ tim-ryan/weather_tim.js
@@ -1,57 +1,56 @@
-var sat_View = 'mapbox://styles/mapbox/satellite-v9';
-var street_View = 'mapbox://styles/mapbox/streets-v9';
 
-
+/*--Creates a Map---------------------------------------------------------------------------------------- */
 mapboxgl.accessToken = 'pk.eyJ1IjoidGltcnlhbjEzMyIsImEiOiJjanN6MnJ0a3cwMWQyNGFsZTZwdWhzaXkzIn0.85NLdpDSOWhF_CPb_SLRBw';
 var map = new mapboxgl.Map({
-    container: 'map',
-    style: street_View,
-    center: [-98.4951,  29.4246],
-    zoom: 10
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+    center: [-74.50, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
 });
+
+/*--Console log the lat and lon of the mouse movement---------------------------------------------------------------------------------------- */
+// map.on('mousemove', e => {
+//     console.log("move", e.lngLat);
+// });
+
+/*--Console log the lat and long of a mouse click---------------------------------------------------------------------------------------- */
+// map.on('click', e => {
+//     console.log("click", e.lngLat);
+//     const placemarker = e.lngLat.toArray();
+//     console.log(placemarker);
+// });
+
+/*--Create a popup on the map---------------------------------------------------------------------------------------- */
+// const popup = new mapboxgl.Popup();
+// popup.setLngLat([-74.64488220213762, 40.1702115636993])
+//     .setHTML(`<h1> Mapbox </h1>`)
+//     .addTo(map);
+
+/*--Create a popup on the map at each mouse click---------------------------------------------------------------------------------------- */
+// map.on('click', e => {
+//     const popup = new mapboxgl.Popup();
+//     const placemarker = e.lngLat.toArray();
+//     console.log(placemarker);
+//     popup.setLngLat(placemarker)
+//         .setHTML(`<h1> Mapbox </h1>`)
+//         .addTo(map);
+// });
+
 
 var geocoder = new MapboxGeocoder({
     accessToken: mapboxgl.accessToken
 });
 
+var popup = new mapboxgl.Popup({ offset: 100 })
+    .setText('Construction on the Washington Monument began in 1848.');
+
 map.addControl(geocoder);
 
-// After the map style has loaded on the page, add a source layer and default
-// styling for a single point.
-map.on('load', function() {
-    map.addSource('single-point', {
-        "type": "geojson",
-        "data": {
-            "type": "FeatureCollection",
-            "features": []
-        }
-    });
-
-    map.addLayer({
-        "id": "point",
-        "source": "single-point",
-        "type": "circle",
-        "paint": {
-            "circle-radius": 10,
-            "circle-color": "#007cbf"
-        }
-    });
-
-// Listen for the `result` event from the MapboxGeocoder that is triggered when a user
-// makes a selection and add a symbol that matches the result.
     geocoder.on('result', function(ev) {
-        map.getSource('single-point').setData(ev.result.geometry);
+        new mapboxgl.Marker()
+            .setLngLat(ev.result.center)
+            .setPopup(popup)
+            .addTo(map);
     });
-});
 
-/*---Button that toggles between satalite view and street view--------------------------------------------------------------------------------------- */
-
-var toggle_Base_Map = document.getElementById("toggle_Base_Map");
-
-$(toggle_Base_Map).click(
-
-    function() {
-        console.log($(toggle_Base_Map))
-    }
-);
 
